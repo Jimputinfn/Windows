1394da28488aae11ae75822e7934396cac7ea74f
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-28 17:53:31 +0000

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,10 @@
+#installs
+
+winget install Ablaze.Floorp
+winget install Valve.Steam
+winget install Discord.Discord
+winget install Microsoft.VisualStudio.2022.Community
+winget install nepnep.neofetch-win
+winget install EpicGames.EpicGamesLauncher
+winget install CalcProgrammer1.OpenRGB
+
